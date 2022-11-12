8993499858e79f04be25bf2f836598b6dae1eefe
mortadha1144 <mortadha114@gmail.com>
2022-11-13 00:42:46 +0300

    lesson 74

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -2,83 +2,101 @@ import 'package:bmi_a/shared/components/components.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-   LoginScreen({super.key}) ;
+  LoginScreen({super.key});
+
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  onEditingComplete: () {},
-                  keyboardType: TextInputType.emailAddress,
-                  onFieldSubmitted: (value) => print(value),
-                  onChanged: (value) => print(value),
-                  decoration: const InputDecoration(
-                    labelText: 'Email Address',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.email),
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  onEditingComplete: () {},
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  onFieldSubmitted: (value) => print(value),
-                  onChanged: (value) => print(value),
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: Icon(Icons.remove_red_eye),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                defaultButton(onPressed: () {
-                  print(emailController.text);
-                  print(passwordController .text);
-                }, text: 'login'),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Don\'t have an account ?'),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('Register Now'),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                )
-              ],
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  defaultFormField(
+                    controller: emailController,
+                    type: TextInputType.emailAddress,
+                    label: 'Email',
+                    prefixIcon: Icons.email,
+                    validate: (value) {
+                      if((value??'').isEmpty){
+                        return'email must not be empty';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  defaultFormField(
+                    controller: passwordController,
+                    type: TextInputType.visiblePassword,
+                    label: 'Password',
+                    prefixIcon: Icons.lock,
+                    suffixIcon: Icons.remove_red_eye,
+                    isPassword: true,
+                    validate: (value) {
+                      if((value??'').isEmpty){
+                        return'password is too short';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  defaultButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        print(emailController.text);
+                        print(passwordController.text);
+                      }
+                    },
+                    text: 'login',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  defaultButton(
+                    onPressed: () {
+                      print(emailController.text);
+                      print(passwordController.text);
+                    },
+                    text: 'ReGIster',
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Don\'t have an account ?'),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('Register Now'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
 
 Widget defaultButton({
-  double width=double.infinity,
-  Color background=Colors.blue,
+  double width = double.infinity,
+  Color background = Colors.blue,
+  bool isUpperCase = true,
+  double radius = 10,
   required Function() onPressed,
   required String text,
-})=>Container(
-  width: width,
-  height: 40,
-  color: background,
-  child: MaterialButton(
-    onPressed: onPressed,
-    child:  Text(
-      text.toUpperCase(),
-      style: const TextStyle(color: Colors.white),
-    ),
-  ),
-);
+}) =>
+    Container(
+      width: width,
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        color: background,
+      ),
+      child: MaterialButton(
+        onPressed: onPressed,
+        child: Text(
+          isUpperCase ? text.toUpperCase() : text,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+
+Widget defaultFormField({
+  required TextEditingController controller,
+  required TextInputType type,
+  void Function(String)? onSubmit,
+  void Function(String)? onChange,
+  bool isPassword = false,
+  required String? Function(String?)? validate,
+  required String label,
+  required IconData prefixIcon,
+  IconData? suffixIcon,
+}) =>
+    TextFormField(
+      controller: controller,
+      keyboardType: type,
+      obscureText: isPassword,
+      onFieldSubmitted: onSubmit,
+      onChanged: onChange,
+      validator: validate,
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(),
+        prefixIcon: Icon(prefixIcon),
+        suffixIcon: Icon(suffixIcon),
+      ),
+    );
